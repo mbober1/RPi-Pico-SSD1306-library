302fed7cfb0999734a5f2bac986e41dc678aee81
Marcin Bober <mbober1@gmail.com>
2021-04-22 17:33:27 +0200

    description update

--- GFX.cpp
+++ GFX.cpp
@@ -1,9 +1,22 @@
 #include "GFX.hpp"
 
-
+/**
+ * Create GFX instantion 
+ *
+ * @param DevAddr device i2c address.
+ * @param Size screen size (W128xH64 or W128xH32)
+ * @param i2c i2c instance
+ */
 GFX::GFX(uint16_t const DevAddr, size Size, i2c_inst_t * i2c) : SSD1306(DevAddr, Size, i2c) {};
 
-
+/**
+ * Draw one char.
+ *
+ * @param x position from the left edge (0, MAX WIDTH)
+ * @param y position from the top edge (0, MAX HEIGHT)
+ * @param chr char to be written
+ * @param color colors::BLACK, colors::WHITE or colors::INVERSE
+ */
 void GFX::drawChar(int x, int y, char chr, colors color)
 {
 	if(chr > 0x7E) return; // chr > '~'
@@ -22,7 +35,14 @@ void GFX::drawChar(int x, int y, char chr, colors color)
     }
 }
 
-
+/**
+ * Draw string.
+ *
+ * @param x position from the left edge (0, MAX WIDTH)
+ * @param y position from the top edge (0, MAX HEIGHT)
+ * @param str string to be written
+ * @param color colors::BLACK, colors::WHITE or colors::INVERSE
+ */
 void GFX::drawString(int x, int y, std::string str, colors color)
 {
 	int x_tmp = x;
@@ -36,23 +56,51 @@ void GFX::drawString(int x, int y, std::string str, colors color)
 }
 
 
+/**
+ * Draw empty rectangle.
+ *
+ * @param x position from the left edge (0, MAX WIDTH)
+ * @param y position from the top edge (0, MAX HEIGHT)
+ * @param w width of the rectangle
+ * @param h height of the rectangle
+ * @param color colors::BLACK, colors::WHITE or colors::INVERSE
+ */
 void GFX::drawRectangle(int x, int y, uint16_t w, uint16_t h, colors color)
 {
-    this->drawFastHLine(x, y, w, color);
-    this->drawFastHLine(x, y+h-1, w, color);
-    this->drawFastVLine(x, y, h, color);
-    this->drawFastVLine(x+w-1, y, h, color);
+    this->drawHorizontalLine(x, y, w, color);
+    this->drawHorizontalLine(x, y+h-1, w, color);
+    this->drawVerticalLine(x, y, h, color);
+    this->drawVerticalLine(x+w-1, y, h, color);
 }
 
 
+/**
+ * Draw filled rectangle.
+ *
+ * @param x position from the left edge (0, MAX WIDTH)
+ * @param y position from the top edge (0, MAX HEIGHT)
+ * @param w width of the rectangle
+ * @param h height of the rectangle
+ * @param color colors::BLACK, colors::WHITE or colors::INVERSE
+ */
 void GFX::drawFillRectangle(int x, int y, uint16_t w, uint16_t h, colors color)
 {
     for (int i=x; i<x+w; i++) {
-    	this->drawFastVLine(i, y, h, color);
+    	this->drawVerticalLine(i, y, h, color);
     }
 }
 
 
+/**
+ * Draw progress bar.
+ *
+ * @param x position from the left edge (0, MAX WIDTH)
+ * @param y position from the top edge (0, MAX HEIGHT)
+ * @param w width of the rectangle
+ * @param h height of the rectangle
+ * @param progress progress (0, 100)
+ * @param color colors::BLACK, colors::WHITE or colors::INVERSE
+ */
 void GFX::drawProgressBar(int x, int y, uint16_t w, uint16_t h, uint8_t progress, colors color)
 {
     this->drawRectangle(x, y, w, h, color);
@@ -60,56 +108,81 @@ void GFX::drawProgressBar(int x, int y, uint16_t w, uint16_t h, uint8_t progress
 }
 
 
-void GFX::drawFastVLine(int x_start, int y_start, int h, colors color)
+/**
+ * Draw vertical line.
+ *
+ * @param x position from the left edge (0, MAX WIDTH)
+ * @param y position from the top edge (0, MAX HEIGHT)
+ * @param h height of the line
+ * @param color colors::BLACK, colors::WHITE or colors::INVERSE
+ */
+void GFX::drawVerticalLine(int x, int y, int h, colors color)
 {
-	this->writeLine(x_start, y_start, x_start, y_start+h-1, color);
+	this->writeLine(x, y, x, y+h-1, color);
 }
 
 
-void GFX::drawFastHLine(int x_start, int y_start, int w, colors color)
+/**
+ * Draw horizontal line.
+ *
+ * @param x position from the left edge (0, MAX WIDTH)
+ * @param y position from the top edge (0, MAX HEIGHT)
+ * @param w width of the line
+ * @param color colors::BLACK, colors::WHITE or colors::INVERSE
+ */
+void GFX::drawHorizontalLine(int x, int y, int w, colors color)
 {
-	this->writeLine(x_start, y_start, x_start+w-1, y_start, color);
+	this->writeLine(x, y, x+w-1, y, color);
 }
 
 
+/**
+ * Draw straight line.
+ *
+ * @param x_start position of the first point from the left edge (0, MAX WIDTH)
+ * @param y_start position of the first point from the top edge  (0, MAX HEIGHT)
+ * @param x_end position of the second point from the left edge (0, MAX WIDTH)
+ * @param y_end position of the second point from the top edge  (0, MAX HEIGHT)
+ * @param color colors::BLACK, colors::WHITE or colors::INVERSE
+ */
 void GFX::writeLine(int x_start, int y_start, int x_end, int y_end, colors color)
 {
 	int16_t steep = abs(y_end - y_start) > abs(x_end - x_start);
 
-	    if (steep) {
-	        _swap_int(x_start, y_start);
-	        _swap_int(x_end, y_end);
-	    }
+	if (steep) {
+		this->swap(x_start, y_start);
+		this->swap(x_end, y_end);
+	}
 
-	    if (x_start > x_end) {
-	        _swap_int(x_start, x_end);
-	        _swap_int(y_start, y_end);
-	    }
+	if (x_start > x_end) {
+		this->swap(x_start, x_end);
+		this->swap(y_start, y_end);
+	}
 
-	    int16_t dx, dy;
-	    dx = x_end - x_start;
-	    dy = abs(y_end - y_start);
+	int16_t dx = x_end - x_start;
+	int16_t dy = abs(y_end - y_start);
 
-	    int16_t err = dx / 2;
-	    int16_t ystep;
+	int16_t err = dx / 2;
+	int16_t ystep;
 
-	    if (y_start < y_end) {
-	        ystep = 1;
-	    } else {
-	        ystep = -1;
-	    }
+	if (y_start < y_end) ystep = 1;
+	else ystep = -1;
 
-	    for (; x_start<=x_end; x_start++) {
-	        if (steep) {
-	        	this->drawPixel(y_start, x_start, color);
-	        } else {
-	        	this->drawPixel(x_start, y_start, color);
-	        }
-	        err -= dy;
-	        if (err < 0) {
-	            y_start += ystep;
-	            err += dx;
-	        }
-	    }
+	while(x_start <= x_end) {
+		if (steep) this->drawPixel(y_start, x_start, color);
+		else this->drawPixel(x_start, y_start, color);
+		x_start++;
+		err -= dy;
+		if (err < 0) {
+			y_start += ystep;
+			err += dx;
+		}
+	}
 }
 
+
+inline void GFX::swap(int &a, int &b) {
+    int tmp = a;
+    a = b;
+    b = tmp;
+}
--- GFX.hpp
+++ GFX.hpp
@@ -8,11 +8,12 @@
 #include <stdlib.h>
 #include <string>
 
-#define _swap_int(a, b) { int t = a; a = b; b = t; }
 
 
 class GFX : public SSD1306 {
     const uint8_t* font = font_8x5;
+
+    static void swap(int &a, int &b);
 
     public:
         GFX(uint16_t const DevAddr, size Size, i2c_inst_t * i2c);
@@ -22,8 +23,8 @@ class GFX : public SSD1306 {
         void drawProgressBar(int x, int y, uint16_t w, uint16_t h, uint8_t progress, colors color = colors::WHITE);
         void drawFillRectangle(int x, int y, uint16_t w, uint16_t h, colors color = colors::WHITE);
         void drawRectangle(int x, int y, uint16_t w, uint16_t h, colors color = colors::WHITE);
-        void drawFastHLine(int x_start, int y_start, int w, colors color = colors::WHITE);
-        void drawFastVLine(int x_start, int y_start, int w, colors color = colors::WHITE);
+        void drawHorizontalLine(int x, int y, int w, colors color = colors::WHITE);
+        void drawVerticalLine(int x, int y, int w, colors color = colors::WHITE);
         void writeLine(int x_start, int y_start, int x_end, int y_end, colors color = colors::WHITE);
 
 };
